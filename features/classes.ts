de4ce15c0f9bd5d6f2cd0e225dbbb40a0ef6037d
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-12-25 13:36:54 +0500

    Add modifiers

--- features/classes.ts
+++ features/classes.ts
@@ -1,19 +1,19 @@
 class Vehicle {
-  drive(): void {
-    console.log("chugga chugga")
-  }
-
-  honk(): void {
+  protected honk(): void {
     console.log("beep")
   }
 }
 
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log("vroom")
+  }
+  startDrivingProcess(): void {
+    this.drive()
+    this.honk()
   }
 }
 
 const car = new Car()
-car.drive()
+car.startDrivingProcess()
 car.honk()
